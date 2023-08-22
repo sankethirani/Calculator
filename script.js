93c03664b936fb84e696acdf3a0f49b81ecfbfbc
Sanket <sankethirani.sh@gmail.com>
2023-08-23 11:00:58 +1200

    Add functionality for operator buttons

--- script.js
+++ script.js
@@ -15,14 +15,74 @@ function divide(num1, num2){
 }
 
 function operate(num1, num2, operator){
+    num1 = Number(num1);
+    num2 = Number(num2);
     switch (operator){
-        case "+":
+        case '+':
             return add(num1,num2);
-        case "-":
+        case '-':
             return subtract(num1, num2);
-        case "*":
+        case '*':
             return multiply(num1, num2);
-        case "/":
+        case '÷':
             return divide(num1, num2);
     }
 }
+
+function appendNumber(){
+    if (bottomScreen.textContent === '0'){
+        bottomScreen.textContent = '';
+    }
+    bottomScreen.textContent += this.dataset.number;
+}
+
+function setOperator(){
+    if (topScreen.textContent === ''){
+        operator = this.dataset.operator;
+        firstOperand = bottomScreen.textContent;
+        topScreen.textContent = `${firstOperand} ${operator}`;
+        bottomScreen.textContent = '';
+    }
+    else{
+        if (operator != null){
+            if (bottomScreen.textContent == ''){
+                operator = this.dataset.operator;
+                topScreen.textContent = `${firstOperand} ${operator}`;
+            }
+            else{
+                secondOperand = bottomScreen.textContent;
+                calculate(this.dataset.operator);
+            }
+        }
+        if (operator === null && bottomScreen.textContent === ''){
+            operator = this.dataset.operator;
+            topScreen.textContent = `${firstOperand} ${operator}`;
+        }
+    }
+    
+}
+
+function calculate(calcSign){
+    result = operate(firstOperand,secondOperand,operator);
+    operator = calcSign;
+    topScreen.textContent = `${result} ${operator}`;
+    bottomScreen.textContent = '';
+    firstOperand = result;
+    secondOperand = '';
+    
+}
+
+
+
+
+let operator = null;
+let firstOperand = '';
+let secondOperand = '';
+
+const bottomScreen = document.getElementById('bottom-screen');
+const topScreen = document.getElementById('top-screen');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+
+numberButtons.forEach (button => button.addEventListener('click', appendNumber));
+operatorButtons.forEach (button => button.addEventListener('click', setOperator));
